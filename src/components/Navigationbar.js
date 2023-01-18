319a579e07ad75fa494c01edf128dd907ad0036f
cojder <cojder997@gmail.com>
2023-01-18 14:14:15 +0100

    Initial commit

---
+++ src/components/Navigationbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navigationbar = () => {
+  return (
+    <div>
+      <div>hamburg menue</div>
+      <div>logo</div>
+      <div>Saznaj gde je najbliza Tvoja kuhinjica</div>
+      <div>uloguj se</div>
+    </div>
+  );
+};
+
+export default Navigationbar;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navigationbar from "../components/Navigationbar";
+import Footer from "../components/Footer";
+
+const HomePage = () => {
+  return (
+    <>
+      <Navigationbar />
+      <Footer />
+    </>
+  );
+};
+
+export default HomePage;
